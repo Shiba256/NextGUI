0574d4a2352636d0822c67b14afaab025783391e
Shiba256 <153472750+Tomoyuki-Kawaguchi@users.noreply.github.com>
2024-12-18 12:57:15 +0900

    Mainの更新

--- NextGUI/Main.cpp
+++ NextGUI/Main.cpp
@@ -1,5 +1,26 @@
 ﻿# include <Siv3D.hpp>
 
-void Main(){
+#include"Button.h"
 
+void Main(){
+	Scene::SetBackground(Palette::Aliceblue);
+	nGUI::ButtonStyle style{
+		nGUI::StyleBase{
+			.pos = Scene::CenterF(),
+			.size = {200,50},
+			.position_type = nGUI::PositionType::center,
+		}
+	};
+
+
+	style.setR(10);
+	style.text = nGUI::Text{
+		.text = U"こんにちは",
+	};
+	nGUI::Button button{ style };
+
+	while (System::Update()) {
+		button.update();
+		button.draw();
+	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # NextGUI
+
+## コンポーネントスタイル共通メンバ
+- SizeF size
+- Vec2 pos
+- int32 z_index
+
+## コンポーネント共通メソッド
+- const Vec2& getPos() const
+- const SizeF& getSize() const
+- constexpr int32 getZIndex() const
