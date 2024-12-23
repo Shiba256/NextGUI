63e3e5eeb862134b64aa2d0ceb5d1faa1e17f3e2
Shiba256 <153472750+Tomoyuki-Kawaguchi@users.noreply.github.com>
2024-12-23 21:49:21 +0900

    定数の追加

---
+++ NextGUI/src/include/Constants.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#ifndef _NEXT_GUI_CONSTANTS_H_
+#define _NEXT_GUI_CONSTANTS_H_
+
+namespace nGUI {
+	namespace Constants {
+		constexpr const char32* MOUSE_EVENT_MANAGER_STR{ U"MouseEventManager" };
+	}
+}
+
+#endif
